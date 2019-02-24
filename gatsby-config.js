65d271a6c7fcd6e394f4afd5cf1af1f057f5ec2f
Naoto Ikeno <naoto.ikeno@dena.com>
2019-02-25 00:07:42 +0900

    auto-convert link in markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
     "gatsby-plugin-typescript",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
+    "gatsby-plugin-catch-links",
     {
       resolve: `gatsby-transformer-remark`,
       options: {
