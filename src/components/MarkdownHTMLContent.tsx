88e1ac1654a07c914bce71adfc8683e70515b0bf
Naoto Ikeno <naoto.ikeno@dena.com>
2019-02-25 00:01:23 +0900

    update

--- src/components/MarkdownHTMLContent.tsx
+++ src/components/MarkdownHTMLContent.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 
 import * as Prism from "prismjs";
+import "prismjs/components/prism-java";
+import "prismjs/components/prism-vim";
+import "prismjs/components/prism-perl";
 import "./prism.css";
+import "./prism-custom.sass";
 
 const MarkdownHTMLContent = class extends React.Component<{ content: string }> {
   componentDidMount() {
