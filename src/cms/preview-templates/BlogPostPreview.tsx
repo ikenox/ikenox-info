4bf18d7d159fc7c5cec8b2d8d24bf359dc32d5c8
Naoto Ikeno <naoto.ikeno@dena.com>
2019-03-01 23:26:20 +0900

    update

--- src/cms/preview-templates/BlogPostPreview.tsx
+++ src/cms/preview-templates/BlogPostPreview.tsx
@@ -1,8 +1,17 @@
 import * as React from "react";
+import { BlogPostTemplate } from "../../templates/blog-post";
 
 const BlogPostPreview: React.FC<{
   entry: { getIn: Function };
   widgetFor: Function;
-}> = props => props.widgetFor("body");
+}> = props => (
+  <BlogPostTemplate
+    content={props.widgetFor("body")}
+    date={props.entry.getIn(["data", "date"])}
+    description={props.entry.getIn(["data", "description"])}
+    tags={props.entry.getIn(["data", "tags"])}
+    title={props.entry.getIn(["data", "title"])}
+  />
+);
 
 export default BlogPostPreview;
